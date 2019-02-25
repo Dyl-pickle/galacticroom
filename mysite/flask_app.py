25b8cd7db5e28635a426706342a8b65ba3b82f08
dckoldan2 <dckoldan2@cougars.ccis.edu>
2019-02-25 18:15:36 +0000

    This is a test

---
+++ mysite/flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="GalacticRoom",
+    password="student.347",
+    hostname="GalacticRoom.mysql.pythonanywhere-services.com",
+    databasename="GalacticRoom$default",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+@app.route('/login', methods=['GET', 'POST'])
+def galactic_room():
+    error = None
+    if request.method == 'POST':
+        if request.form['uname'] != 'admin' or request.form['psw'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
